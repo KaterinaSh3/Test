7196cc2e98a6e13b746b0451a11d17d6019b09db
Ekaterina Shevchenko <ekaterina.shevchenko@finteco.io>
2022-07-13 21:44:14 +0300

    height added

--- Program.cs
+++ Program.cs
@@ -10,13 +10,14 @@ namespace Lesson2
             string surename = "";
             int age = i;
             int responce = 0;             //@ дает вывести буквально как написано не разбивая по отдельным командам на каждую строку, не нужно писать консолрайтлайн каждый раз
-            while (responce ! = -1)            //можно складывать строки "Enter 0 to enter name"+ Environment.NewLine+"Enter yoyr..."+Environment.NewLine+"..." - будет как с @                  
-                Enter 1 to enter surename
+            int height = 0;     //add for height_adding
+            while (responce != -1)            //можно складывать строки "Enter 0 to enter name"+ Environment.NewLine+"Enter yoyr..."+Environment.NewLine+"..." - будет как с @                  
             {
                 Console.WriteLine(@"Enter 0 to enter name                   
                                     Enter 1 to enter surename
                                     Enter 2 to enter age
                                     Enter -2 to pint resume
+                                    Enter 4 to enter height
                                     Enter -1 to exit");
                 Console.WriteLine();
                 responce = int.Parse(Console.ReadLine());
@@ -30,7 +31,7 @@ namespace Lesson2
                         }
                     case 1:
                         {
-                            Console.WrieLine("Enter your surename");
+                            Console.WriteLine("Enter your surename");
                             surename = Console.ReadLine();
                             break;
                         }
@@ -40,32 +41,21 @@ namespace Lesson2
                             age = int.Parse(Console.ReadLine());
                             break;
                         }
-                    default:
+                    case 3:
                         {
-                            global::System.Console.WriteLine("enter correct unswer");
+                            Console.WriteLine("Enter your height");
+                            height = int.Parse(Console.ReadLine());
                             break;
                         }
-                if (responce==0)
-                {
+                    default:
+                        {
+                            global::System.Console.WriteLine("enter correct answer");
+                            break;
+                        }
                     Console.WriteLine("Enter your name");
-                    name = Console.ReadLine();
-                }
-                if(responce == 1)
-                {
-                    Console.WrieLine("Enter your surename");
-                    surename = Console.ReadLine();
-                }
-                if (responce==2)
-                {
+                    Console.WriteLine("Enter your surename");
                     Console.WriteLine("Enter your age");
-                    age = Console.ReadLine();
-                }
-                if (responce == -2)
-                {
-
-                    Console.WriteLine("Enter your name");
-                    Console.WrieLine("Enter your surename");
-                    Console.WrieLine("Enter your age");
+                    Console.WriteLine("Enter your height");
                 }
             }
             Console.WriteLine(name);
